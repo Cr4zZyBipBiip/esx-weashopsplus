8039fe646e3eb58c72c5b6a33a3ad4d7a5e7c127
Lance Good <lgoodyt@gmail.com>
2018-03-25 11:21:56 -0400

    Update esx_weashops+.sql

--- esx_weashops+.sql
+++ esx_weashops+.sql
@@ -1,37 +1,6 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               5.7.21-log - MySQL Community Server (GPL)
--- Server OS:                    Win64
--- HeidiSQL Version:             9.5.0.5196
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
-
--- Dumping database structure for essentialmode
-CREATE DATABASE IF NOT EXISTS `essentialmode` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `essentialmode`;
 
--- Dumping structure for table essentialmode.licenses
-CREATE TABLE IF NOT EXISTS `licenses` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `type` varchar(255) NOT NULL,
-  `label` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
-
--- Dumping data for table essentialmode.licenses: ~3 rows (approximately)
-/*!40000 ALTER TABLE `licenses` DISABLE KEYS */;
 INSERT INTO `licenses` (`id`, `type`, `label`) VALUES
 	(0, 'weapon', 'Weapon Level: One'),
 	(1, 'weapon2', 'Weapon Level: Two'),
 	(2, 'weapon3', 'Weapon Level: Three');
-/*!40000 ALTER TABLE `licenses` ENABLE KEYS */;
-
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
